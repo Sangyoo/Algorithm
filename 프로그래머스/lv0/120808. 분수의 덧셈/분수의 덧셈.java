cd4a3890d02d0bf430238fd6750537a4aa9aacfb
milLee <tkddbwkd@naver.com>
2023-03-19 13:48:04 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        // 최대공약수 구하기
+        int p = gcd(denom1, denom2);
+        // if(denom1 < denom2 ){
+        //     p = gcd(denom2, denom1);
+        // }
+        // else if (denom1 > denom2) {
+        //     p = gcd(denom1, denom2);
+        // }
+        // else p = denom1;
+        
+        // 최소공배수 구하기
+        int q = denom1 * denom2 / p;
+        // 분자의 합
+        int z = ((q / denom2) * numer2) + ((q / denom1) * numer1);
+        
+        // 기약분수로 나타내기
+        int r = gcd(z,q);
+        if( r > 1) {
+            return new int[]{z/r,q/r};
+        }
+        return new int[]{z,q};
+        
+        
+    }
+    public static int gcd(int a, int b) {
+        if(a > b) {
+            if( b == 0) return a;
+            return gcd(b, a%b);
+        }
+        if( a < b) {
+            if( a == 0) return b;
+            return gcd(a, b%a);
+        }
+        else return a;
+    }
+}
