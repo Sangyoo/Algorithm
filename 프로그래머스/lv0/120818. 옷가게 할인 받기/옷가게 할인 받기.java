a8f882568597bf4342b857d94750b24d60caa8b2
milLee <tkddbwkd@naver.com>
2023-03-20 12:06:30 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        if(100000 > price ){
+            answer = price;
+        }
+        else if(100000 <= price && price < 300000){
+            answer = (int) (price * 0.95);
+        }
+        else if(300000 <= price && price < 500000){
+            answer = (int) (0.9 * price);
+        }
+        else if(500000 <= price){
+            answer =  (int) (price * 0.8);
+        }
+        return answer;
+    }
+}
